05110a3688bd64bae91acd4c12db2a6409115708
lindexi <lindexi_gd@163.com>
2024-11-21 17:49:35 +0800

    修复构建工具没有执行替换

--- src/dotnetCampus.AppHost/Program.cs
+++ src/dotnetCampus.AppHost/Program.cs
@@ -1,20 +1,28 @@
-﻿#if !NET8_0
+﻿#if !IS_EXE_TOOL
+using System;
+
 internal static class Program
 {
     private static void Main()
     {
+        throw new NotSupportedException("不支持 .NET 8.0 以下的版本。通常是 NuGet 包制作错误，如看到此异常，请到 https://github.com/dotnet-campus/dotnetCampus.AppHost 提 Issues");
     }
 }
 #else
+using System;
 using dotnetCampus.AppHosting.Tasks;
 using dotnetCampus.Cli;
 using dotnetCampus.MSBuildUtils;
 
 try
 {
+    Console.WriteLine(new MSBuildMessage($"开始执行 dotnet campus AppHost 替换工作。命令行参数：{Environment.CommandLine}").ToString(MessageLevel.Message));
+
     var options = CommandLine.Parse(args)
         .AddHandler<AppHostPatchingTask>(o => o.Run())
         .Run();
+
+    Console.WriteLine(new MSBuildMessage("完成 dotnet campus AppHost 替换工作").ToString(MessageLevel.Message));
 }
 catch (MSBuildException ex)
 {
